3d239746f5bcdcd2b13c95be4731cb39fadcb5e1
anish <ansghimire321@gmail.com>
2022-08-24 15:55:17 +0545

    yarn

--- src/components/Aboutpage.js
+++ src/components/Aboutpage.js
@@ -5,11 +5,11 @@ const Aboutpage = () => {
         <div id="about" className="container h-[90vh] flex space-x-60 mb-16">
             <div className="  mt-20 ml-36">
                 <h1 className="text-white text-3xl tracking-wider">1. About Me</h1>
-                <h1 className='text-lg mt-8 w-[30rem] text-gray-400'>
-                    Hi, I'm Anish Ghimire, a Fullstack Developer From Chitwan,Nepal. I'm a self taught Developer. I love to explore new technology and skills. Although I have no experience with world project work in a company,I have done many projects.
+                <h1 className="text-lg mt-8 w-[30rem] text-gray-400">
+                    Hi, I"m Anish Ghimire, a Fullstack Developer From Chitwan,Nepal. I"m a self taught Developer. I love to explore new technology and skills. Although I have no experience with world project work in a company,I have done many projects.
                 </h1>
-                <h1 className='text-lg mt-8 w-[30rem] text-gray-400'>
-                    Here are few technologies that I've been working with recently.
+                <h1 className="text-lg mt-8 w-[30rem] text-gray-400">
+                    Here are few technologies that I"ve been working with recently.
                 </h1>
                 <div className="flex space-x-20 text-white mt-5 font-mono">
                     <ul className="flex flex-col space-y-3 ">
